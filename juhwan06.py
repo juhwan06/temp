29b49fa0c75eb8c90d189e334271e8f3f39d31a5
juhwan06 <53961923+juhwan06@users.noreply.github.com>
2019-08-20 19:07:59 +0900

    Add files via upload

---
+++ juhwan06.py
@@ -0,0 +1,23 @@
+import random
+
+# 데이터 종류 : 정수, 소수, 문자, 참/거짓
+
+a = []
+for q in range(10):
+    flag = 0
+    if (q == 0):
+        a.append(random.randrange(1, 10))
+    else:
+        b = random.randrange(1, 10)
+        for j in range(len(a)):
+            if (b == a[j]):
+                flag = 1
+                break
+        if (flag != 1):
+            a.append(b)
+c = a[0]
+for i in range(len(a)):
+    if(c < a[i]):
+        c = a[i]
+print(a)
+print(c)
